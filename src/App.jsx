a921b9f17fa19ed8e21576cb1572f4b6fa21c515
Kreya Bansal <kreyabansal@Kreyas-MacBook-Air.local>
2026-09-26 01:05:26 +0200

    Modified to show todays session as text (hardcoded)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+const sessions = [
+  { start: "09:00", end: "10:30", activity: "University" },
+  { start: "10:30", end: "11:00", activity: "YouTube" },
+  { start: "11:00", end: "12:00", activity: "Programming" },
+  { start: "13:00", end: "14:00", activity: "Lunch"}
+]
+
 function App() {
   return (
     <div>
-      <h1> TimeRun </h1>
-      <p> Easiest way to keep track of where your time is going!</p>
+      <h1>TimeRun</h1>
+      <p>Easiest way to keep track of where your time is going!</p>
+
+      <h2>Today</h2>
+      <ul>
+        {sessions.map((s) => (
+          <li key={s.start}>
+            {s.start} → {s.end} {s.activity}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
